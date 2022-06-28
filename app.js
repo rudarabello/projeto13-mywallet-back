32adc02bd7cfbb9b4de502de6135c77abeab35c8
rudarabello <ruda.rabello.silva@gmail.com>
2022-06-28 10:38:23 -0300

    feat: add post on '/' and '/singn-up'

--- app.js
+++ app.js
@@ -11,4 +11,44 @@ app.use(cors());
 
 const client = new MongoClient(process.env.MONGO_URL);
 let db;
-client.connect(()=>{db = client.db('dataBank')});
+client.connect(() => { db = client.db('dataBank') });
+
+app.post("/sign-up", async (req, res) => {
+    const user = req.body.user;
+    const email = req.body.email;
+    const cpf = req.body.cpf;
+    const password = req.body.password;
+    try {
+        const newUser = {
+            user: user,
+            email: email,
+            cpf: cpf,
+            password: password
+        }
+        await db.collection("users").insertOne(newUser);
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
+});
+
+app.post("/", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const confirmationEmail = await db.collection("users").findOne({ email: email });
+        const confirmationpassword = await db.collection("users").findOne({ password: password });
+        if (confirmationEmail && confirmationpassword) {
+            //devolver um token para o front
+            res.sendStatus(201);
+            return;
+        } else {
+            res.sendStatus(409).send("Usuário não cadastrado!");
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
+
+app.listen(5000);
