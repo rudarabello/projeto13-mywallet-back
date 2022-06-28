1ab829e34cc0658bbf061d24e89a7b5e00712c2d
rudarabello <ruda.rabello.silva@gmail.com>
2022-06-28 14:29:33 -0300

    feat: add controllers as architecture

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+import { postSignUp, postLogin } from "./src/controllers/post-controller.js";
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,46 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
 const client = new MongoClient(process.env.MONGO_URL);
 let db;
 client.connect(() => { db = client.db('dataBank') });
 
-app.post("/sign-up", async (req, res) => {
-    const user = req.body.user;
-    const email = req.body.email;
-    const cpf = req.body.cpf;
-    const password = req.body.password;
-    try {
-        const newUser = {
-            user: user,
-            email: email,
-            cpf: cpf,
-            password: password
-        }
-        await db.collection("users").insertOne(newUser);
-        res.sendStatus(201);
-    } catch (error) {
-        res.sendStatus(500);
-    }
+app.post("/sign-up", (req, res) => postSignUp(req, res, db));
 
-});
-
-app.post("/", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    try {
-        const confirmationEmail = await db.collection("users").findOne({ email: email });
-        const confirmationpassword = await db.collection("users").findOne({ password: password });
-        if (confirmationEmail && confirmationpassword) {
-            //devolver um token para o front
-            res.sendStatus(201);
-            return;
-        } else {
-            res.sendStatus(409).send("Usuário não cadastrado!");
-        }
-    } catch (error) {
-        res.sendStatus(500);
-    }
-});
+app.post("/", (req, res) => postLogin(req, res, db));
 
 app.listen(5000);
---
+++ src/controllers/post-controller.js
@@ -0,0 +1,40 @@
+
+async function postSignUp(req, res, db) {
+    const user = req.body.user;
+    const email = req.body.email;
+    const cpf = req.body.cpf;
+    const password = req.body.password;
+    try {
+        const newUser = {
+            user: user,
+            email: email,
+            cpf: cpf,
+            password: password
+        }
+        await db.collection("users").insertOne(newUser);
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+};
+
+async function postLogin(req, res, db) {
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const confirmationEmail = await db.collection("users").findOne({ email: email });
+        const confirmationpassword = await db.collection("users").findOne({ password: password });
+        if (confirmationEmail && confirmationpassword) {
+            //devolver token para o front
+            res.sendStatus(201);
+            return;
+        } else {
+            res.sendStatus(409).send("Usuário não cadastrado!");
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+};
+
+
+export { postSignUp, postLogin };
