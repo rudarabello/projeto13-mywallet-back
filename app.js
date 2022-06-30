a335b7573aafef958ba315600f2629f4f8ce7258
rudarabello <ruda.rabello.silva@gmail.com>
2022-06-30 18:49:18 -0300

    fix: port-server

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-import { postSignUp, postLogin } from "./src/controllers/post-controller.js";
+import { postSignUp, postLogin, postTransaction} from "./src/controllers/post-controller.js";
+import { getHome,  } from "./src/controllers/get-controllers";
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const app = express();
 app.use(express.json());
 app.use(cors());
 
+const app = express();
 
-
-const client = new MongoClient(process.env.MONGO_URL);
 let db;
+const PORT = process.env.PORT || 5009;
+const client = new MongoClient(process.env.MONGO_URL);
 client.connect(() => { db = client.db('dataBank') });
 
 app.post("/sign-up", (req, res) => postSignUp(req, res, db));
 
-app.post("/", (req, res) => postLogin(req, res, db));
+app.post("/login", (req, res) => postLogin(req, res, db));
 
-app.listen(5000);
+app.get("/home", (req, res) => getHome(req, res, db));
+
+app.post("/transactions", (req, res) => postTransaction(req, res, db));
+
+app.listen(PORT);
