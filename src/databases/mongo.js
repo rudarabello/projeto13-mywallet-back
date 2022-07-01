7062fd8219e12981b49ec28f9304f572b68e0184
rudarabello <ruda.rabello.silva@gmail.com>
2022-07-01 11:11:25 -0300

    fix: change db export on mongo.js

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-
-let db;
 dotenv.config();
+
 const client = new MongoClient(process.env.MONGO_URL);
-client.connect(() => { db = client.db('mywallet') });
+client.connect();
+export let db = client.db('mywallet');
